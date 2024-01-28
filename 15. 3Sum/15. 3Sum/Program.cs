a1a401043ca6a373139b642fff166586d2beca36
Sohil abuzeid <103829138+Sohil423Abuzeid@users.noreply.github.com>
2024-01-29 01:50:01 +0200

    11. Container With Most Water

---
+++ 15. 3Sum/15. 3Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _15._3Sum
+{
+    public class Solution
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            Dictionary<int,int>nums_counter= new Dictionary<int, int>();
+            List<int> uniqe_nums = new List<int>();
+            foreach (int i in nums)
+                if (nums_counter.ContainsKey(i))
+                    nums_counter[i]++;
+                else
+                {
+                    uniqe_nums.Add(i);
+                    nums_counter[i]= 1;
+                }
+            for (int i = 0; i < uniqe_nums.Count; i++)
+                    
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
