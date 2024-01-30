12610796bb7747767d0a0ca19e111a640c9f9d9e
Sohil abuzeid <103829138+Sohil423Abuzeid@users.noreply.github.com>
2024-01-31 01:55:13 +0200

    Time: 309 ms (10.95%), Space: 66.1 MB (73.30%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cs
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cs
@@ -4,6 +4,8 @@ public class Solution
     public int FindDuplicate(int[] nums)
     {
        var a= nums.GroupBy(x => x).Where(g=> g.Count() > 1).Select(g => new { Item = g.Key, Frequency = g.Count() });
-        return a.First().Item ;
+        int aa = a.First().Item;
+        GC.Collect();
+        return aa;
     }
 }
