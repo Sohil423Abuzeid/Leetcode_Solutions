d335106c349789c6212bd2f7f5f420abb214dd4c
Sohil abuzeid <103829138+Sohil423Abuzeid@users.noreply.github.com>
2024-02-02 03:50:48 +0200

    Update 1498-Number-of-Subsequences-That-Satisfy-the-Given-Sum-Condition.cs
    upload the code for some reasons the tool didnt upload it

--- 1498-Number-of-Subsequences-That-Satisfy-the-Given-Sum-Condition.cs
+++ 1498-Number-of-Subsequences-That-Satisfy-the-Given-Sum-Condition.cs
@@ -1,1 +1,29 @@
-null
+public class Solution {
+   public int NumSubseq(int[] nums, int target)
+{
+    int mod = 1000000007;
+    Dictionary<int, int> calco = new Dictionary<int, int>();
+    int ss = 1;
+    for (int s = 0; s <= 100000; s++)
+    {
+        calco[s] = ss;
+        ss = (ss * 2) % mod;
+    }
+    Array.Sort(nums);
+    int i = 0, o = nums.Length - 1, ans = 0;
+    while (i <= o)
+    {
+        while (o - 1 >= i && nums[o] + nums[i] > target) o--;
+        if (nums[o] + nums[i] <= target)
+        {
+
+            //else ans = (ans + ((i + 1) * (i+2 )) % mod) % mod;
+            // ans = (ans + (i >= 0 ? calco[i] : 0))%mod;
+            ans = (ans + (o - i >= 0 ? calco[o - i] : 0)) % mod;
+        }
+
+        i++;
+    }
+    return ans;
+}
+}
