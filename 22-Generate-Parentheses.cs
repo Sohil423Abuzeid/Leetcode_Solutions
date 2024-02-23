5bb1f48f22c7a3181dff1ec74d949001f69ad7a6
Sohil abuzeid <103829138+Sohil423Abuzeid@users.noreply.github.com>
2024-02-24 00:13:57 +0200

    [22] [Time Beats: 19.79%] [Memory Beats: 68.98%] - LeetPush

--- 22-Generate-Parentheses.cs
+++ 22-Generate-Parentheses.cs
@@ -1,44 +1,25 @@
 public class Solution {
-    int count(int a)
+   int nn;
+List<string> result = new List<string>();
+void build(string s , int a ,int b)
 {
-    int b = 0;
-    while (a>0)
-    {
-        b++;
-        a = a & (a - 1);
+    if (a > nn || b > nn||b>a) return;
+    if (a == b&&a==nn) { 
+        result.Add(s);
+        build(s + \(\, a + 1, b);
+        return;
     }
-    return b;
-}
-Stack<int> tester = new Stack<int>();
-bool right(string a)
-{
-    tester.Clear();
-    for (int i = 0; i < a.Length; i++)
+    else
     {
-        if(tester.Count == 0) tester.Push(a[i]);
-        else if (tester.Peek() != a[i] && a[i]==')') tester.Pop();
-        else tester.Push(a[i]);
+        build(s + \(\, a + 1, b);
+        build(s + \)\, a , b+1);
     }
-    return tester.Count == 0;
+
 }
 public IList<string> GenerateParenthesis(int n)
-{ 
-    List<string> result = new List<string>();  
-    n *= 2;
-    for (int i = 1; i < 1<<n; i++)
-        if(count(i)==n/2)
-            {
-                string s = \\;
-                char a = '(',b=')';
-                if (i % 2 == 1) (a, b) = (b, a);
-                for (int o = 0; o < n; o++)
-                    if ((i & (1 << o)) > 0)
-                        s = a + s;
-                    else
-                        s = b + s;
-                if(!result.Contains(s)&& right(s))
-                result.Add(s);
-            }    
+{
+    nn = n;
+    build(\(\, 1, 0);
     return result;
 }
 }
