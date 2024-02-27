b361c30d56a3753c14fb203d86e287abbaaf66ef
Sohil abuzeid <103829138+Sohil423Abuzeid@users.noreply.github.com>
2024-02-27 20:45:14 +0200

    [704] [Time Beats: 28.46%] [Memory Beats: 7.01%] - LeetPush

---
+++ 704-Binary-Search.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int Search(int[] nums, int target)
+{
+    int start =0 , end = nums.Length-1,mid;
+    while(start <= end)
+    {
+        mid = (start+end)/2;
+        if (nums[mid] == target) return mid;
+        else if (nums[mid] > target) end = mid - 1;
+        else start = mid + 1;
+    }
+    return -1;
+}
+}
