bfb2a8c45ed2c36b9e8cac9020e7c40a0869f944
Sohil abuzeid <103829138+Sohil423Abuzeid@users.noreply.github.com>
2024-04-24 06:11:41 +0200

    [1137] [Time Beats: 74.66%] [Memory Beats: 32.76%] - LeetPush

--- 1137-N-th-Tribonacci-Number.cs
+++ 1137-N-th-Tribonacci-Number.cs
@@ -1,9 +1,10 @@
 public class Solution {
     public int Tribonacci(int n) {
-        int[] asd = new int[38];
+        if(n<=2) return n>0?1:0;
+        int[] asd = new int[n+1];
         asd[0]=0;
         asd[2]=asd[1]=1;
-        for(int i =3 ; i< 38 ; i++){
+        for(int i =3 ; i< n+1 ; i++){
             asd[i]=asd[i-1]+asd[i-2]+asd[i-3];
         }
         return asd[n];
