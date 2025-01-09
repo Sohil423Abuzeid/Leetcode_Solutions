dfdb76a9e13b61943412409b4b1e7ddb3e683558
Sohil abuzeid <103829138+Sohil423Abuzeid@users.noreply.github.com>
2025-01-09 21:36:56 +0200

    [620] [Time Beats: 88.76%] - LeetPush

--- 620-Not-Boring-Movies.sql
+++ 620-Not-Boring-Movies.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select * from Cinema where MOD(id, 2) = 1 and description != 'boring' order by rating DESC
+select * from Cinema where MOD(id, 2) != 0 and description != 'boring' order by rating DESC
