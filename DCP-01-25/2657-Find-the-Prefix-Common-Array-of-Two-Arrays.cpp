8309dc42aab0ce91bed0d0a86ef7ea61f519b6f3
Sohil abuzeid <103829138+Sohil423Abuzeid@users.noreply.github.com>
2025-01-15 00:57:44 +0200

    [2657] [Time Beats: 15.67%] [Memory Beats: 5.15%] - LeetPush

---
+++ DCP-01-25/2657-Find-the-Prefix-Common-Array-of-Two-Arrays.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        map<int,bool>inA,inB,Done;
+        vector<int> ans (A.size());
+        for(int i =0 ; i < A.size();i++){
+            ans[i]=i-1>=0?ans[i-1]:0;
+            if(inB[A[i]]&&!Done[A[i]])
+                ans[i]++,Done[A[i]]=true;
+            inA[A[i]]=true;
+            if(inA[B[i]]&&!Done[B[i]])
+                ans[i]++,Done[B[i]]=true;
+            inB[B[i]]=true;
+        } 
+        return ans;
+    }
+};
