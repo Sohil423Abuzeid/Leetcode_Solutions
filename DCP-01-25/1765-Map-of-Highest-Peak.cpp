1fb87de5565ed76f6548a3796c22e5c265926ad0
Sohil abuzeid <103829138+Sohil423Abuzeid@users.noreply.github.com>
2025-01-23 00:59:39 +0200

    [1765] [Time Beats: 54.33%] [Memory Beats: 86.11%] - LeetPush

--- DCP-01-25/1765-Map-of-Highest-Peak.cpp
+++ DCP-01-25/1765-Map-of-Highest-Peak.cpp
@@ -11,35 +11,31 @@ private:
 public:
     vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
         queue<pair<int,int>> bfs;
-        map<pair<int,int>,bool>visted ;
         int n = isWater.size(),m = isWater[0].size();
         for(int i = 0 ; i <n;i++)
             for(int o=0; o<m ;o++)
                 if(isWater[i][o]==1)
                 {
                     bfs.push({i,o});
-                    isWater[i][o]=0;
-                    visted[{i,o}]=true;
                 }
-        int ans=0;
         while(bfs.size()>0)
         {
             pair<int,int> temp = bfs.front();
             int x =temp.first;
             int y= temp.second;
             bfs.pop();
-            ans = max(isWater[x][y],ans);
             for(int i = 0 ; i < 4 ; i++)
             {
-                if(valid(x+dx[i],y+dy[i],n,m)&&!visted[{x+dx[i],y+dy[i]}])
+                if(valid(x+dx[i],y+dy[i],n,m)&& isWater[x+dx[i]][y+dy[i]]==0)
                     {
                         bfs.push({x+dx[i],y+dy[i]});
                         isWater[x+dx[i]][y+dy[i]]=isWater[x][y]+1;
-                        visted[{x+dx[i],y+dy[i]}]=true;
                     }
             }
         }
-        cout << ans;
+        for(int i = 0 ; i <n;i++)
+            for(int o=0; o<m ;o++)
+                    isWater[i][o]--;
         return isWater;
     }
 };
